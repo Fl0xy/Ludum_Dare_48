fdbaf16bf119d7731e5de7bcf2f4dd577a638343
fl0xy <florian@deeken.net>
2021-04-24 15:14:58 +0200

    deep fryer sounds V1

--- components/deep_fryer/DeepFryer.gd
+++ components/deep_fryer/DeepFryer.gd
@@ -1,41 +1,49 @@
 extends Node2D
 
-var power: bool = true
+var power: bool = true setget setPower
 var randomPowerFlicker: float
 
-var heaterPowered: bool = true
+var heaterPowered: bool setget setHeaterPower
 var randomHeaterFlicker: float
 
 func _ready():
+	power = true
 	randomPowerFlicker = float(15 + (randi() % 30))
+	heaterPowered = true
 	randomHeaterFlicker = float(60 + (randi() % 10))
 	
 
 func _physics_process(delta):
 	randomPowerFlicker -= delta
-	randomHeaterFlicker -= delta
-	
 	if (randomPowerFlicker <= 0):
 		if power:
 			randomPowerFlicker = 0.30 + (randi() % 3) * 0.16
-			power = false
-			$LightGreen.visible = false
-			$LightRed.visible = false
+			setPower(false)
 		else:
 			randomPowerFlicker = float(15 + (randi() % 30))
-			power = true
-			$LightGreen.visible = true
-			if heaterPowered:
-				$LightRed.visible = true
+			setPower(true)
 	
+	if !power:
+		return
+		
+	randomHeaterFlicker -= delta
 	if (randomHeaterFlicker <= 0):
 		if heaterPowered:
 			randomHeaterFlicker = 5 + (randi() % 5)
-			heaterPowered = false
-			$LightRed.visible = false
+			setHeaterPower(false)
+			
 		else:
 			randomHeaterFlicker = 10 + (randi() % 5)
-			heaterPowered = true
-			$LightRed.visible = true
-
-
+			setHeaterPower(true)
+	
+func setHeaterPower(state: bool):
+	heaterPowered = state
+	$AudioRelay.play()
+	$LightRed.visible = state
+	
+func setPower(state: bool):
+	power = state
+	$LightGreen.visible = state
+	setHeaterPower(state)
+	
+	
