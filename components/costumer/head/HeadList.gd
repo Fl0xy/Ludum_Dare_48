2f6d55111685c92ad9488e7cf6d630b1c91044cc
akronsus <no>
2021-04-25 10:59:43 +0200

    integrated first head and body parts

--- components/costumer/head/HeadList.gd
+++ components/costumer/head/HeadList.gd
@@ -2,7 +2,8 @@ extends Node
 
 
 var headList: Array = [
-	preload("res://components/costumer/head/PlaceholderHead.tscn")
+	preload("res://components/costumer/head/PlaceholderHead.tscn"),
+	preload("res://components/costumer/head/head_1.tscn")
 ]
 
 
--- components/costumer/shirt/ShirtList.gd
+++ components/costumer/shirt/ShirtList.gd
@@ -2,7 +2,8 @@ extends Node
 
 
 var shirtList: Array = [
-	preload("res://components/costumer/shirt/PlaceholderShirt.tscn")
+	preload("res://components/costumer/shirt/PlaceholderShirt.tscn"),
+	preload("res://components/costumer/shirt/body_1.tscn")
 ]
 
 
